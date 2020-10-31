70fc707308e5469823e5e57591d5f294a317c2e7
Vik Borges <v1k@protonmail.com>
2020-10-31 20:57:23 +0000

    make phone number optional & added some data

--- src/resumes/material-dark.vue
+++ src/resumes/material-dark.vue
@@ -30,7 +30,7 @@
       </div>
     </div>
 
-    <a :href="contactLinks.phone">
+    <a v-if="person.contact.phone" :href="contactLinks.phone">
       <div class="item">
         <div class="icon">
           <i class="material-icons">phone</i>
@@ -119,7 +119,7 @@
           <div class="subheadline">{{experience.timeperiod}}</div>
           <p class="info">
             {{experience.description}}
-          </p>  
+          </p>
       </a>
     </div>
     <div class="section-headline">{{ lang.education }}</div>
@@ -474,7 +474,7 @@ h4 {
       }
     }
   }
-  .item.last{ 
+  .item.last{
     .text {
       border-bottom-style:none;
       padding-bottom:0;
